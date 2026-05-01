573512b4d9dcf72e31839eb602dfa53935a259dd
kusatarou936 <kusatarou.936@gmail.com>
2026-05-01 18:18:54 +0900

    fix sitemap base variable

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -21,7 +21,7 @@ export async function GET() {
             .map(
                 (u) => `
       <url>
-        <loc>${base}${u}</loc>
+        <loc>${BASE}${u}</loc>
       </url>
     `
             )
