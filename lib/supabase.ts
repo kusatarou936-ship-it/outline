7161e506407c11b1a8a0d036b853e660bb7672bf
kusatarou936 <kusatarou.936@gmail.com>
2026-05-04 10:00:24 +0900

    update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const server = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
-
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -14,4 +9,4 @@ export const supabase = createClient(
       autoRefreshToken: true
     }
   }
-)
+);
