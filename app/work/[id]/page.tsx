80a781f2ca01d7b911ceefa5d6c536ae1cefb149
kusatarou936 <kusatarou.936@gmail.com>
2026-05-06 14:44:36 +0900

    update

--- app/work/[id]/page.tsx
+++ app/work/[id]/page.tsx
@@ -3,9 +3,9 @@
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import type { Work, Comment } from "@/app/types";
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-const supabase = createClient(
+const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
