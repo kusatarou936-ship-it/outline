652489151f2ef3c648d0c1465db8e7d84fd5287f
kusatarou936 <kusatarou.936@gmail.com>
2026-05-06 18:09:56 +0900

    update

--- app/components/LangSwitcher/LangSwitcher.tsx
+++ app/components/LangSwitcher/LangSwitcher.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useI18n } from "@/lib/i18n";
+import { useI18n } from "../../../lib/i18n";
 
 export default function LangSwitcher() {
   const { lang, setLang } = useI18n();
