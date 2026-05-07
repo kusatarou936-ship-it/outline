a86dec3d47854dc6a8352b4f7e3afccc9b828691
kusatarou936 <kusatarou.936@gmail.com>
2026-05-07 13:30:59 +0900

    update

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const accessToken =
-    req.cookies.get("sb-access-token")?.value ||
-    req.cookies.get("__Host-sb-access-token")?.value;
+    const accessToken =
+        req.cookies.get("sb-access-token")?.value ||
+        req.cookies.get("__Host-sb-access-token")?.value;
 
-  if (!accessToken) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+    if (!accessToken) {
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
 
-  return NextResponse.next();
+    return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/(auth)/(.*)"],
+    matcher: ["/submit/:path*"],
 };
