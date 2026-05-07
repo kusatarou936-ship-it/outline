b5ab8a0bad975ec2b32d22eece487fb7ab84a642
kusatarou936 <kusatarou.936@gmail.com>
2026-05-07 15:33:39 +0900

    update

---
+++ app/test-auth/page.tsx
@@ -0,0 +1,14 @@
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export default async function Page() {
+  const cookieStore = cookies();
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies: { get: (n) => cookieStore.get(n)?.value } }
+  );
+
+  const { data } = await supabase.auth.getUser();
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+}
