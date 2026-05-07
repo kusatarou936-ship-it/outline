6f2398f1ff1017aaebeb87846b77378cbe242f83
kusatarou936 <kusatarou.936@gmail.com>
2026-05-07 16:23:50 +0900

    fix: remove Suspense from login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,8 @@
-import { Suspense } from "react";
 import LoginClient from "./LoginClient";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return (
-    <Suspense>
-      <LoginClient />
-    </Suspense>
-  );
+  return <LoginClient />;
 }
