2f47393e14241112e3c046e3669b75947eea6d0e
kusatarou936 <kusatarou.936@gmail.com>
2026-05-07 14:53:51 +0900

    update

--- app/submit/external/page.tsx
+++ app/submit/external/page.tsx
@@ -1,10 +1,14 @@
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import ExternalSubmit from "./ExternalSubmit";
+import { NextResponse } from "next/server";
 
 export default async function Page() {
   const cookieStore = cookies();
+  const res = NextResponse.next();
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -15,10 +19,10 @@ export default async function Page() {
           return cookieStore.get(name)?.value;
         },
         set(name: string, value: string, options: any) {
-          cookieStore.set({ name, value, ...options });
+          res.cookies.set(name, value, options);
         },
         remove(name: string, options: any) {
-          cookieStore.set({ name, value: "", ...options });
+          res.cookies.set(name, "", options);
         },
       },
     }
--- app/submit/internal/page.tsx
+++ app/submit/internal/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
